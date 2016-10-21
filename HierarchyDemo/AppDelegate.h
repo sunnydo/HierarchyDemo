2a31548d4202b2018923eca29b4db5caab30c7a0
sunnydo <sunnydo@sunnys-imac.local>
2016-10-21 12:04:31 +0800

    Initial Commit

---
+++ HierarchyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HierarchyDemo
+//
+//  Created by Sunny Do on 21/10/2016.
+//  Copyright © 2016 Sunny Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HierarchyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HierarchyDemo
+//
+//  Created by Sunny Do on 21/10/2016.
+//  Copyright © 2016 Sunny Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
